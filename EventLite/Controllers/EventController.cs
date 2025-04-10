5b2f386f6e8665d4f7b2a64387405e5f6728e7ac
Chriswalt64 <walters.chris64@gmail.com>
2025-04-10 15:24:42 -0400

    - Added Events View - Seeded db - basic setup

---
+++ EventLite/Controllers/EventController.cs
@@ -0,0 +1,21 @@
+﻿using EventLite.Data;
+using EventLite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventLite.Controllers
+{
+    public class EventController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        public EventController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Event> objEventList = _db.Events.ToList();
+            return View(objEventList);
+        }
+    }
+}
